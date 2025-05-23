bcb7fb0c045b387f5549f77a4a04438cbde54be1
sdb0605@naver.com <sdb0605@naver.com>
2025-05-24 02:17:26 +0900

    git create

--- tmp.tsx
+++ tmp.tsx
@@ -1,1 +1,9 @@
-hello world!
+git을 로컬에 설치해서 업로드 하는 방법
+
+vsCode에 폴더 or 파일을 생성
+git init
+git add .
+git commit -m "커밋 메세지"
+git remote add origin "레포지토리 위치"
+git push -u origin master //로컬 프랜치에 push
+
